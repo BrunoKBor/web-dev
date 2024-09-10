f497c5d975959550897ff558014a19717e035e62
BrunoKBor <brunoborkem@gmail.com>
2024-09-10 23:33:21 +0300

    minor test

--- Backend/2.3 NPM/index.js
+++ Backend/2.3 NPM/index.js
@@ -1,13 +1,13 @@
 // var generateName = require('sillyname');
 
-// import generateName from "sillyname";
-// var sillyName = generateName();
+import generateName from "sillyname";
+var sillyName = generateName();
 
-// console.log(`My name is ${sillyName}.`);
+console.log(`My silly name is ${sillyName}.`);
 
 import superheroes from 'superheroes';
 
 // Generate a random superhero name
 const superheroName = superheroes.random();
 
-console.log(`My name is ${superheroName}.`);
+console.log(`My  superhero name is ${superheroName}.`);
