8a9c4ab27fc2c4b9330c0e9475cd853c67c4fe12
BrunoKBor <brunoborkem@gmail.com>
2024-10-07 07:10:08 +0300

    complete ejs challenge

--- 28 - Challenge 3 - Blog Website/ejs-challenge/app.js
+++ 28 - Challenge 3 - Blog Website/ejs-challenge/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,9 +16,13 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts = [];
+
 app.get("/", function(req, res) {
   res.render("home", {
-    startingContent: homeStartingContent
+    startingContent: homeStartingContent,
+    posts: posts,
+    _ : _
   });
 });
 
@@ -38,16 +43,34 @@ app.get("/compose", function(req, res) {
 });
 
 app.post("/compose", function(req, res) {
-  const postContent = req.body.postContent;
-  const postTitle = req.body.postTitle;
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postContent
+  };
+
+  posts.push(post);
+  res.redirect("/");
   
-  console.log("Title: "+postTitle);
-  console.log("Content: "+postContent);
-  res.send(`<script>alert("Published!"); window.location.href = "/compose";</script>`);
+  // res.send(`<script>alert("Published!"); window.location.href = "/";</script>`);
   
 });
 
+app.get('/posts/:postsName', (req, res) => {
+  const requestedTitle = _.lowerCase(req.params.postsName);// Normalize the URL param to lowercase using lodash
 
+  let foundPost = posts.find(post => _.lowerCase(post.title) === requestedTitle); 
+
+  if (foundPost) {
+    console.log("Match found!");
+    res.render("post", { 
+      title: foundPost.title, 
+      content: foundPost.content 
+    }); 
+  } else {
+    console.log("No match found!");
+    res.send("No matching post found!"); 
+  }
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
